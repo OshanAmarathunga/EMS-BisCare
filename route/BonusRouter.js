ba27bb8e68d98ad019a224890857437daa3e8dbe
oshanamarathunga <oshanamarathunga1@gmail.com>
2025-09-11 23:34:44 +0530

    commit

--- route/BonusRouter.js
+++ route/BonusRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getBonus, saveBonus} from "../controller/BonusController.js";
+import {changeStatus, getBonus, saveBonus} from "../controller/BonusController.js";
 
 const router=express.Router();
 
 router.post('/save-bonus',saveBonus);
 router.get('/get-bonus/:id',getBonus);
+router.put('/change-status/:id',changeStatus);
 
 export default router;
